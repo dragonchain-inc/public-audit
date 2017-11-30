c42edac00b3d99c5cec43c339e8c338931006986
Alex Benedetto <alex061994@gmail.com>
2017-11-29 19:20:24 -0800

    added hw swaps and splitting for groups

--- global_variables.js
+++ global_variables.js
@@ -16,7 +16,7 @@ var csv_loader = function(file,callback){
         if(err) {
           console.log(err);
           callback(err);
-        } 
+        }
         var headers = data[0];
         var list = [];
         for(var i=1; i<data.length; i+=1){
@@ -78,7 +78,7 @@ var presale_loader = function(arr){
     temp['txid'] = null;
     temp['type'] = arr[i].type;
     temp[arr[i].type] = arr[i].amount;
-    temp['convert_rate'] = presale_close_ethbtc;
+    temp['convert_rate'] = (temp['type']=='btc')?1:presale_close_ethbtc;
     temp['ethereum_payout_address'] = null;
     temp['btc_value'] = (arr[i].type == 'btc')?arr[i].amount.toString():new Decimal(arr[i].amount).mul(presale_close_ethbtc).toString();
     temp['%'] = null;
@@ -89,6 +89,92 @@ var presale_loader = function(arr){
   }
   return final;
 }
+
+// convert_rate is optional
+var convert_ethereum = function(data,bittrex_hourly,convert_rate_input=null){
+  var final_array = [];
+  for(var i=0; i<data.length; i+=1){
+    var temp = data[i];
+    if(convert_rate_input){
+      temp['btc_value'] = Decimal.mul(temp['eth'],convert_rate_input).toString();
+    } else {
+      // convert amount and return
+      var timestamp = new Date(parseInt(temp['unix-epoch']));
+      timestamp = new Date(timestamp).setHours(new Date(timestamp).getHours() + 1);
+      timestamp = new Date(timestamp).setMinutes(0);
+      timestamp = new Date(timestamp).setSeconds(0);
+      timestamp = new Date(timestamp).toISOString();
+      // lookup conversion and insert
+      var convert_rate = bittrex_hourly[timestamp.split('.')[0]];
+      temp['amount_btc'] = Decimal.mul(temp['amount'], convert_rate).toString();
+      temp['convert_rate'] = convert_rate;
+    }
+    final_array.push(temp);
+  }
+  return final_array;
+}
+
+// arr1 = split1, arr2 = split2
+var apply_splits = function(idx,arr,txid,splits){
+  var origin_transaction = arr[idx];
+  var type = origin_transaction['type'];
+  var origin_percent = new Decimal(origin_transaction['%']).div(100).toString();
+  var total = new Decimal(0);
+  var total_temp_percent = new Decimal(0);
+  var filtered = null;
+  var final = [];
+  var split_out = splits[txid];
+
+  // filters out the splits that only deal with either bitcoin or ethereum
+  if(type=='eth') {
+    filtered = split_out.filter(eth_filter);
+    //convert
+    filtered = convert_ethereum(filtered,null,origin_transaction['convert_rate']);
+  }
+  else filtered = split_out.filter(btc_filter);
+
+  // calculate the total bitcoin value for the split array
+  for(var i=0; i<filtered.length; i+=1){
+    if(type=='eth') total = total.plus(filtered[i]['btc_value']);
+    else total = total.plus(filtered[i]['btc']);
+  }
+  for(var i=0; i<filtered.length; i+=1){
+    var temp = {};
+    temp['exception'] = origin_transaction['exception'];
+    temp['exception_type'] = origin_transaction['exception_type'];
+    temp[type] = filtered[i][type];
+    temp['timestamp'] = origin_transaction['timestamp'];
+    temp['unix-epoch'] = origin_transaction['unix-epoch'];
+    temp['txid'] = origin_transaction['txid'];
+    temp['type'] = origin_transaction['type'];
+    temp['ethereum_payout_address'] = (filtered[i]['ethereum_payout_address'])?filtered[i]['ethereum_payout_address']:null;
+    temp['convert_rate'] = origin_transaction['convert_rate'];
+    temp['btc_value'] = (type=='eth')?filtered[i]['btc_value']:filtered[i]['btc'];
+    // find percentage of current transaction against total_btc_value
+    var temp_percent = new Decimal(temp['btc_value']).div(total);
+    temp['%'] = temp_percent.mul(origin_percent).mul(100).toString();
+    temp['dragons'] = Decimal.mul(temp_percent,origin_transaction['dragons']).toPrecision(18).toString();
+    total_temp_percent  = total_temp_percent.plus(temp_percent);
+    final.push(temp);
+  }
+  return final;
+}
+
+var handle_hardware_swap = function(audit,swaps){
+  var final = [];
+  for(var i=0; i<audit.length; i+=1){
+    var temp = audit[i];
+    if(swaps.hasOwnProperty(temp['ethereum_payout_address']) && temp['exception'] != "yes"){
+      console.log('Swapping from address: '+temp['ethereum_payout_address']+' to address: '+swaps[temp['ethereum_payout_address']]);
+      temp['ethereum_payout_address'] = swaps[temp['ethereum_payout_address']];
+    }
+    final.push(temp);
+  }
+  return final;
+}
+
+var eth_filter = function(x){ return x['eth']>0; }
+var btc_filter = function(x){ return x['btc']>0; }
 
 var sort_unix = function(a,b){return a['unix-epoch'] - b['unix-epoch'];}
 
@@ -113,21 +199,24 @@ var cash_contributions = [
   [4040,'0xb3b7aC365fbb97E67F30074A042423C9A04e0451'],
   [530,'0x3ff7E67B452bCa45E009328022bC1B84bB8ce7CD']];
 
-var presale_contributions = [{type: 'btc',amount: 152.25},{type: 'eth',amount: 2357.53}]; // results from the presale
+var presale_contributions = [{type: 'btc',amount: 157.25},{type: 'eth',amount: 2357.53}]; // results from the presale
 var presale_close_ethbtc  = 0.076320; // close conversion rate for August 22, 2017 at 6:00:00 PM PDT
 
-module.exports.csv_loader   = csv_loader;
-module.exports.write_csv    = write_csv;
-module.exports.sort_unix    = sort_unix;
-module.exports.format_cash  = format_cash;
-module.exports.presale_load = presale_loader;
-module.exports.start_date   = START_DATE;
-module.exports.end_date     = END_DATE;
-module.exports.presale_date = PRESALE_DATE;
-module.exports.end_btc_price= END_BTC_PRICE;
-module.exports.audit_excp   = audit_exceptions_file;
-module.exports.cash_contr   = cash_contributions;
-module.exports.presale_contr= presale_contributions;
-module.exports.presale_close= presale_close_ethbtc;
-module.exports.public_drgns = public_dragons;
-module.exports.cash_close   = CASH_CLOSE;
+module.exports.csv_loader       = csv_loader;
+module.exports.write_csv        = write_csv;
+module.exports.sort_unix        = sort_unix;
+module.exports.format_cash      = format_cash;
+module.exports.presale_load     = presale_loader;
+module.exports.apply_splits     = apply_splits;
+module.exports.convert_ethereum = convert_ethereum;
+module.exports.handle_hw_swaps  = handle_hardware_swap;
+module.exports.start_date       = START_DATE;
+module.exports.end_date         = END_DATE;
+module.exports.presale_date     = PRESALE_DATE;
+module.exports.end_btc_price    = END_BTC_PRICE;
+module.exports.audit_excp       = audit_exceptions_file;
+module.exports.cash_contr       = cash_contributions;
+module.exports.presale_contr    = presale_contributions;
+module.exports.presale_close    = presale_close_ethbtc;
+module.exports.public_drgns     = public_dragons;
+module.exports.cash_close       = CASH_CLOSE;
